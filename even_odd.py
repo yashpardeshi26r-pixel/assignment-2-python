d7a2d2f3e50e6790b3f80e0d883470685c569e42
Yash <yashpardeshi26r@gmail.com>
2026-02-18 12:26:41 +0530

    Add files via upload

---
+++ even_odd.py
@@ -0,0 +1,4 @@
+"check if the number is even or odd"
+"by using if else statement"
+num= int(input("enter your number:"))
+print("even")if num%2==0 else print("odd")
---
+++ for_loop.py
@@ -0,0 +1,7 @@
+#for loop
+"sum of integers from 1 to 50 using for loop"
+total=0
+for i in range(1,51):
+    total+=i
+    print("sum of numbers from 1 to 50 is",total)
+
